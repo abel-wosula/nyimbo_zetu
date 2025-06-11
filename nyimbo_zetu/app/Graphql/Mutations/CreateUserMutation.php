9003c33695eafed159f52ed83746175f34cdd61e
stephen <vincentstephen489@gmail.com>
2025-06-11 16:39:00 +0300

    Features: Created user-registration feature ; schema, types and mutations

---
+++ nyimbo_zetu/app/Graphql/Mutations/CreateUserMutation.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Graphql\Mutations;
+
+use App\Models\User;
+use GraphQL\GraphQL;
+use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Support\Facades\Hash;        
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLMutation;
+
+
+class CreateUserMutation{
+    public function createUser($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+
+    {
+        $user=User::create([
+            'email' => $args['email'],
+            'first_name' => $args['first_name'],
+            'last_name' => $args['last_name'],
+            'phone_number' => $args['phone_number'],
+            'address' => $args['address'],
+            'password' => Hash::make($args['password']),
+        ]);
+        return [
+            'user' => $user,
+            'message' => 'User created successfully',
+        ];
+    
+       
+    }
+}
