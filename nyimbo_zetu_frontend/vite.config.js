75d553ea52ddf72b1cd301fd484f47b1ff64009a
Awafula <wafulaabel610@gmail.com>
2025-06-12 21:38:31 +0300

    Accepted modified files

---
+++ nyimbo_zetu_frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import graphqlLoader from "vite-plugin-graphql-loader";
+import vue from "@vitejs/plugin-vue";
+
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools(), tailwindcss(), graphqlLoader()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
