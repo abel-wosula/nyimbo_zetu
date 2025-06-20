73e95fe6724500fd493d2053ac1673f1ac06ca8a
stephen <vincentstephen489@gmail.com>
2025-06-20 14:27:50 +0300

    modified:   nyimbo_zetu/app/Graphql/Mutations/CreateUserMutation.php

--- nyimbo_zetu/app/Graphql/Mutations/CreateUserMutation.php
+++ nyimbo_zetu/app/Graphql/Mutations/CreateUserMutation.php
@@ -24,8 +24,7 @@ class CreateUserMutation
         ]);
         return [
             'user' => $user,
-            'message' => 'User created successfully',
-            
+            'message' => 'User created successfully',        
         ];
     }
 }
