384e46e958e3bec076a6bea6302aaa883ee03156
Awafula <wafulaabel610@gmail.com>
2025-06-23 14:41:10 +0300

    Added songs, category and subcategory tables

---
+++ nyimbo_zetu/database/migrations/2025_06_23_105431_create_subcategory_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcategory', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement(); // Corrected the syntax for auto-incrementing ID
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subcategory');
+    }
+};
---
+++ nyimbo_zetu/database/migrations/2025_06_23_105537_create_songs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('lyrics');
+            $table->string('composer')->nullable();
+            $table->string('midi')->nullable();
+            $table->string('pdf');
+            $table->string('ytlink')->nullable();
+            $table->unsignedBigInteger('subcategory_id');
+            $table->foreign('subcategory_id')->references('id')->on('subcategory')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+};
