49df7769599dec8ecca4895212b798afeebce787
stephen <vincentstephen489@gmail.com>
2025-06-26 11:51:21 +0300

    Feature: Added better styling for the upload and profile button sections

--- nyimbo_zetu_frontend/src/views/profile/index/Main.vue
+++ nyimbo_zetu_frontend/src/views/profile/index/Main.vue
@@ -1,5 +1,15 @@
 <template>
+  <Header />
+
+  <div>
     <div class="flex justify-center pt-20">
-        <h1>Profile page coming soon....</h1>
+      <h1>Coming soon......</h1>
     </div>
+  </div>
+  <Footer />
 </template>
+
+<script setup>
+import Header from "@/components/header/index/Main.vue";
+import Footer from "@/components/footer/index/Main.vue";
+</script>
--- nyimbo_zetu_frontend/src/views/upload/index/Main.vue
+++ nyimbo_zetu_frontend/src/views/upload/index/Main.vue
@@ -1,7 +1,14 @@
 <template>
+    <Header />
     <div>
         <h1 class="flex justify-center pt-20">
             Upload feature coming soon.....
         </h1>
     </div>
+    <Footer />
 </template>
+
+<script setup>
+import Header from '@/components/header/index/Main.vue'
+import Footer from '@/components/footer/index/Main.vue'
+</script>
